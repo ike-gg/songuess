487efec408e2b272e788c3074ef100a392a86508
ike <67127723+ike-gg@users.noreply.github.com>
2023-05-28 09:54:53 +0200

    feat: project config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(black)",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundSize: {
+        screen: "100% 200vh",
+      },
+      animation: {
+        objectPosition: "bgposition 15s ease-in-out infinite alternate",
+        slowlyRotate: "spin 45s linear infinite",
+      },
+      keyframes: {
+        bgposition: {
+          "0%": { objectPosition: "0% 0%" },
+          "100%": { objectPosition: "0% 100%" },
+        },
+        spin: {
+          "0%": { transform: "rotate(0deg)", scale: "200%" },
+          "100%": { transform: "rotate(360deg)", scale: "200%" },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
