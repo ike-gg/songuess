fa2e7b73243d018ee0f2d3dfbd83214679796f51
ike <67127723+ike-gg@users.noreply.github.com>
2023-06-07 10:15:05 +0200

    feat: supabase data types

---
+++ src/types/supabase.ts
@@ -0,0 +1,114 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      countries: {
+        Row: {
+          continent: Database["public"]["Enums"]["continents"] | null
+          id: number
+          iso2: string
+          iso3: string | null
+          local_name: string | null
+          name: string | null
+        }
+        Insert: {
+          continent?: Database["public"]["Enums"]["continents"] | null
+          id?: number
+          iso2: string
+          iso3?: string | null
+          local_name?: string | null
+          name?: string | null
+        }
+        Update: {
+          continent?: Database["public"]["Enums"]["continents"] | null
+          id?: number
+          iso2?: string
+          iso3?: string | null
+          local_name?: string | null
+          name?: string | null
+        }
+      }
+      sets: {
+        Row: {
+          cover: string
+          created_at: string | null
+          description: string | null
+          featured: boolean
+          id: string
+          name: string
+          owner: string
+          songs: string[]
+        }
+        Insert: {
+          cover: string
+          created_at?: string | null
+          description?: string | null
+          featured: boolean
+          id?: string
+          name: string
+          owner: string
+          songs: string[]
+        }
+        Update: {
+          cover?: string
+          created_at?: string | null
+          description?: string | null
+          featured?: boolean
+          id?: string
+          name?: string
+          owner?: string
+          songs?: string[]
+        }
+      }
+      users: {
+        Row: {
+          avatar_url: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+          website: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          id: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          website?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      continents:
+        | "Africa"
+        | "Antarctica"
+        | "Asia"
+        | "Europe"
+        | "Oceania"
+        | "North America"
+        | "South America"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
