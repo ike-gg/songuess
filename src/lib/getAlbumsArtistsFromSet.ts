c740d8a47b3a3848104c647da4a36a433d0e0849
ike <67127723+ike-gg@users.noreply.github.com>
2023-06-07 10:15:01 +0200

    feat: supabase data types

---
+++ src/lib/getAlbumsArtistsFromSet.ts
@@ -0,0 +1,31 @@
+import removeParentheses from "@/utils/removeParentheses";
+
+const getAlbumsArtistsFromSet = async (songsId: string[]) => {
+  const ids = songsId.join(",");
+  const response = await fetch(
+    `https://harmony-backend.vercel.app/api/getSongs?ids=${ids}`
+  );
+  const { data: songDetails } = (await response.json()) as {
+    data: {
+      attributes: {
+        albumName: string;
+        artistName: string;
+      };
+    }[];
+  };
+
+  const albums = [
+    ...new Set(
+      songDetails.map((song) => removeParentheses(song.attributes.albumName))
+    ),
+  ];
+  const artists = [
+    ...new Set(
+      songDetails.map((song) => removeParentheses(song.attributes.artistName))
+    ),
+  ];
+
+  return { albums, artists };
+};
+
+export default getAlbumsArtistsFromSet;
