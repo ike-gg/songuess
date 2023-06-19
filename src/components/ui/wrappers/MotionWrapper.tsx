bc413a24a61830bbd6cc8536bb18b75c82f0516b
ike <67127723+ike-gg@users.noreply.github.com>
2023-06-19 19:40:28 +0200

    feat: motion wrapper

---
+++ src/components/ui/wrappers/MotionWrapper.tsx
@@ -0,0 +1,29 @@
+import { HTMLMotionProps, motion } from "framer-motion";
+import { ReactNode } from "react";
+
+interface Props extends HTMLMotionProps<"div"> {
+  children: ReactNode;
+  distance?: number;
+  delay?: number;
+}
+
+const MotionWrapper = ({
+  children,
+  distance = 20,
+  delay = 0,
+  ...props
+}: Props) => {
+  return (
+    <motion.div
+      initial={{ y: distance, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -distance, opacity: 0 }}
+      transition={{ delay }}
+      {...props}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default MotionWrapper;
