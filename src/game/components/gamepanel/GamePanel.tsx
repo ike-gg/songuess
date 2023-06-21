47cf30fa51a4056d1c5cc69fefbb99b7c6e789fb
ike <67127723+ike-gg@users.noreply.github.com>
2023-06-21 19:41:15 +0200

    feat: game panel components

---
+++ src/game/components/gamepanel/GamePanel.tsx
@@ -0,0 +1,55 @@
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import parseArtwork from "@/utils/parseArtwork";
+import { AnimatePresence, motion } from "framer-motion";
+import { forwardRef } from "react";
+import GuessInput from "../GuessInput";
+import { gameActions } from "@/store/gameSlice";
+import Paragraph from "@/components/ui/content/Paragraph";
+import Heading from "@/components/ui/content/Heading";
+import HeaderPanel from "./HeaderPanel";
+import MotionWrapper from "@/components/ui/wrappers/MotionWrapper";
+import FooterPanel from "./FooterPanel";
+
+const GamePanel = forwardRef<HTMLInputElement>((_, ref) => {
+  const { round, points } = useAppSelector((state) => state.game);
+  const { status, currentSong } = round;
+  const dispatch = useAppDispatch();
+
+  if (!currentSong) return null;
+
+  const { artwork, name, artistName, albumName } = currentSong.attributes;
+  const { bgColor, primColor } = parseArtwork(artwork);
+
+  return (
+    <div className="flex flex-col justify-between gap-4 md:grow">
+      <HeaderPanel />
+      <AnimatePresence mode={"wait"}>
+        {status === "guessing" && (
+          <GuessInput
+            key={"guessInput"}
+            ref={ref}
+            secretPhrase={name}
+            onGuess={() => {
+              dispatch(gameActions.setRoundStatus("guessed"));
+            }}
+            backgroundColor={bgColor}
+            textColor={primColor}
+          />
+        )}
+        {(status === "guessed" || status === "timeout") && (
+          <MotionWrapper>
+            <Heading className="text-inherit">{name}</Heading>
+            <Paragraph className="text-inherit opacity-60">
+              by {artistName} from {albumName}{" "}
+            </Paragraph>
+          </MotionWrapper>
+        )}
+      </AnimatePresence>
+      <FooterPanel />
+    </div>
+  );
+});
+
+GamePanel.displayName = "GamePanel";
+
+export default GamePanel;
