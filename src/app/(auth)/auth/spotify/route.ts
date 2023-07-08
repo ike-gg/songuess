cc2926039dab1a49c202a5515bb6be06c3fc9c97
ike <67127723+ike-gg@users.noreply.github.com>
2023-07-08 07:53:31 +0200

    feat: endpoint for auth spotify

---
+++ src/app/(auth)/auth/spotify/route.ts
@@ -0,0 +1,29 @@
+import { sets } from "@/constants/routes";
+import getAccessTokenSpotify from "@/lib/spotify/getAccessTokenSpotify";
+import { encrypt } from "@/utils/crypto";
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+
+  if (!code) {
+    return NextResponse.redirect(requestUrl.origin + sets.create.blank);
+  }
+
+  try {
+    const data = await getAccessTokenSpotify(code);
+    const { refresh_token } = data;
+
+    const encryptedRefreshToken = encrypt(refresh_token);
+
+    const response = NextResponse.redirect(requestUrl.origin + sets.spotify);
+
+    response.cookies.set("spotify", encryptedRefreshToken);
+    return response;
+  } catch (error) {
+    return NextResponse.redirect(requestUrl.origin + sets.create.blank);
+  }
+}
