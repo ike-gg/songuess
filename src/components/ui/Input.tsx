50db1eb55c2c3a22d4712db9663f48c44246ceeb
ike <67127723+ike-gg@users.noreply.github.com>
2023-07-16 08:03:52 +0200

    feat: input & textarea components

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,42 +1,66 @@
 import { HTMLProps, ReactNode, forwardRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import Tooltip from "./Tooltip";
+import ErrorParagraph from "./content/ErrorParagraph";
+import { RxExclamationTriangle } from "react-icons/rx";
+import { CgSpinner } from "react-icons/cg";
 
 interface Props extends HTMLProps<HTMLInputElement> {
   label: string;
   error?: string;
   className?: string;
   icon?: ReactNode;
+  tooltip?: string;
+  loading?: boolean;
 }
 
 const Input = forwardRef<HTMLInputElement, Props>((inputProps, ref) => {
-  const { label, className, error, icon, ...props } = inputProps;
+  const { label, className, error, icon, tooltip, loading, ...props } =
+    inputProps;
   return (
-    <label className="flex flex-col overflow-hidden">
-      <span className="py-1 text-sm text-zinc-500">{label}</span>
-      <div className="mb-1 flex items-center gap-3 rounded-md border border-zinc-700 bg-zinc-800 p-1 px-3 focus:bg-zinc-900">
-        {icon && <span className="opacity-50">{icon}</span>}
+    <label
+      htmlFor={label}
+      className={twMerge("flex flex-col overflow-hidden", className)}
+    >
+      {label && (
+        <span className="flex gap-1 pb-2 text-sm leading-none text-zinc-600">
+          {label}
+          {tooltip && <Tooltip>{tooltip}</Tooltip>}
+        </span>
+      )}
+      <div
+        className={twMerge(
+          "mb-1 flex items-center gap-3 rounded-md border border-zinc-800/50 bg-zinc-800 p-1 px-3 transition-colors",
+          error && "border-red-900/60 bg-red-900/20"
+        )}
+      >
+        {loading && !error && <CgSpinner className="animate-spin" />}
+        {icon && !error && !loading && (
+          <span className="opacity-50">{icon}</span>
+        )}
+        {error && (
+          <span className="text-red-500 opacity-50">
+            <RxExclamationTriangle />
+          </span>
+        )}
         <input
-          className="grow bg-transparent py-1 text-zinc-200 placeholder:text-zinc-600 "
+          className={twMerge(
+            "grow bg-transparent py-1 text-zinc-200 outline-none transition-colors placeholder:text-zinc-600",
+            error && "text-red-500"
+          )}
           {...props}
           ref={ref}
+          id={label}
         />
       </div>
       <AnimatePresence>
-        {error && (
-          <motion.span
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: "auto", opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
-            className="block text-sm font-semibold text-indigo-500"
-          >
-            • {error}
-          </motion.span>
-        )}
+        {error && <ErrorParagraph className="mt-1">{error}</ErrorParagraph>}
       </AnimatePresence>
     </label>
   );
 });
 
-Input.displayName = "WYPIERDALAJ";
+Input.displayName = "input";
 
 export default Input;
---
+++ src/components/ui/Textarea.tsx
@@ -0,0 +1,47 @@
+import { HTMLProps, ReactNode, forwardRef } from "react";
+import { AnimatePresence } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import ErrorParagraph from "./content/ErrorParagraph";
+import Tooltip from "./Tooltip";
+
+interface Props extends HTMLProps<HTMLTextAreaElement> {
+  label: string;
+  tooltip?: string;
+  error?: string;
+  className?: string;
+  icon?: ReactNode;
+}
+
+const Textarea = forwardRef<HTMLTextAreaElement, Props>((inputProps, ref) => {
+  const { label, className, tooltip, error, icon, ...props } = inputProps;
+
+  return (
+    <label
+      htmlFor={label}
+      className={twMerge("flex flex-col overflow-hidden", className)}
+    >
+      {label && (
+        <span className="flex gap-1 pb-2 text-sm leading-none text-zinc-600">
+          {label}
+          {tooltip && <Tooltip>{tooltip}</Tooltip>}
+        </span>
+      )}
+      <div className="mb-1 flex h-full grow items-center gap-3 rounded-md bg-zinc-800 p-1 px-3 focus:bg-zinc-900">
+        {icon && <span className="opacity-50">{icon}</span>}
+        <textarea
+          className="h-full w-full resize-none bg-transparent py-1 text-zinc-200 outline-none placeholder:text-zinc-600"
+          {...props}
+          ref={ref}
+          id={label}
+        />
+      </div>
+      <AnimatePresence>
+        {error && <ErrorParagraph>{error}</ErrorParagraph>}
+      </AnimatePresence>
+    </label>
+  );
+});
+
+Textarea.displayName = "textarea";
+
+export default Textarea;
