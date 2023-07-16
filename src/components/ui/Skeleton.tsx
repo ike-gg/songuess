259fb961a1e56a8054a8f3533e51c3945882c053
ike <67127723+ike-gg@users.noreply.github.com>
2023-07-16 08:03:19 +0200

    feat: skeleton loading component for loading pages

---
+++ src/components/ui/Skeleton.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from "tailwind-merge";
+
+interface Props {
+  className?: string;
+}
+
+const Skeleton = ({ className }: Props) => {
+  return (
+    <div
+      className={twMerge(
+        "h-4 w-full animate-pulse rounded-md bg-zinc-700",
+        className
+      )}
+    />
+  );
+};
+
+export default Skeleton;
