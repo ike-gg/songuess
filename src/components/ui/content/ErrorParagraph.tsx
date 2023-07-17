961c3ffffe97b4c60f71e5890e2ecd9a5d7f61e1
ike <67127723+ike-gg@users.noreply.github.com>
2023-07-17 08:04:31 +0200

    feat: badge and error paragraph components

---
+++ src/components/ui/content/ErrorParagraph.tsx
@@ -0,0 +1,24 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+
+interface Props {
+  children: string;
+  className?: string;
+}
+
+const ErrorParagraph = ({ children, className }: Props) => {
+  const error = children;
+
+  return (
+    <motion.p
+      initial={{ height: 0, opacity: 0 }}
+      animate={{ height: "auto", opacity: 1 }}
+      exit={{ height: 0, opacity: 0 }}
+      className={twMerge("text-sm font-medium text-red-500", className)}
+    >
+      {error}
+    </motion.p>
+  );
+};
+
+export default ErrorParagraph;
