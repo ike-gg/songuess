993970c73300f1d36ac11bcc5d4e51565a2196b4
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-01 01:32:13 +0200

    feat: set update route

---
+++ src/app/sets/update/loading.tsx
@@ -0,0 +1,34 @@
+import { CardFooter } from "@/components/ui/Card/CardFooter";
+import { Skeleton } from "@/components/ui/Skeleton";
+
+const SetUpdateLoading = async () => {
+  return (
+    <>
+      <div className="flex justify-between">
+        <Skeleton className="w-1/5" />
+        <div className="flex gap-2">
+          <Skeleton className="h-8 w-12" />
+          <Skeleton className="h-8 w-24" />
+        </div>
+      </div>
+      <Skeleton className="h-8 w-2/5" />
+      <Skeleton className="h-52 w-full" />
+      <div className="flex h-fit flex-wrap gap-3 overflow-y-scroll">
+        <Skeleton className="h-[60px] w-24" />
+        <Skeleton className="h-[60px] w-52" />
+        <Skeleton className="h-[60px] w-72" />
+        <Skeleton className="h-[60px] w-40" />
+        <Skeleton className="h-[60px] w-48" />
+        <Skeleton className="h-[60px] w-72" />
+      </div>
+      <CardFooter>
+        <div className="flex justify-end gap-3">
+          <Skeleton className="h-10 w-24" />
+          <Skeleton className="h-10 w-28" />
+        </div>
+      </CardFooter>
+    </>
+  );
+};
+
+export default SetUpdateLoading;
---
+++ src/app/sets/update/page.tsx
@@ -0,0 +1,81 @@
+import SetCreator, {
+  ProvidedValuesSetCreator,
+} from "@/features/sets/components/creator/SetCreator";
+import { Database } from "@/types/supabase";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { SongType } from "@/types/musicApi/Song";
+import { BackButton, Heading } from "@/components/ui";
+import { routes } from "@/constants";
+
+interface SearchParams {
+  playlistid?: string;
+  setid?: string;
+  spotifyplaylistid?: string;
+}
+
+const UpdateSetPage = async ({
+  searchParams: { setid },
+}: {
+  searchParams: SearchParams;
+}) => {
+  const supabase = await createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect(routes.auth.signin);
+  }
+
+  let providedData: ProvidedValuesSetCreator | undefined;
+
+  if (setid) {
+    const { data: existingSet } = await supabase
+      .from("sets")
+      .select("*")
+      .eq("id", setid)
+      .single();
+
+    if (!existingSet) return;
+
+    if (existingSet.owner !== user.id) {
+      redirect(routes.sets.browser);
+    }
+
+    const { name, cover, description, songs, id } = existingSet;
+
+    const response = await fetch(
+      `https://harmony-backend.vercel.app/api/getSongs?ids=${songs.join(",")}`
+    );
+
+    const songsData = (await response.json()) as SongType;
+
+    providedData = {
+      name,
+      playlist: songsData.data,
+      cover: cover || "",
+      description: description || "",
+    };
+  }
+
+  if (!providedData) {
+    redirect(routes.sets.browser);
+  }
+
+  return (
+    <>
+      <nav className="flex items-start justify-between">
+        <BackButton href="/sets">Back to sets</BackButton>
+      </nav>
+      <Heading>
+        Update <b>{providedData.name}</b> set
+      </Heading>
+      <SetCreator existingId={setid} values={providedData} />
+    </>
+  );
+};
+
+export default UpdateSetPage;
