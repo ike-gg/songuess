86327ca06eda9f6e151204ed107d53c4441d05e6
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-01 15:50:00 +0200

    feat: fixed gap for all screens

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -10,7 +10,7 @@ const Card = ({ children, className }: Props) => {
   return (
     <div
       className={twMerge(
-        "relative flex flex-col gap-4 overflow-hidden rounded-xl border border-zinc-800 bg-zinc-900 bg-gradient-to-br p-4 md:gap-5 md:p-6",
+        "relative flex flex-col gap-5 overflow-hidden rounded-xl border border-zinc-800 bg-zinc-900 bg-gradient-to-br p-4 md:p-6",
         className
       )}
     >
