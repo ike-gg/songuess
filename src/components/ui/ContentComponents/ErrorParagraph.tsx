7ab3acdd4c540fc213a111bef9e024e8a8704ea4
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-01 16:24:16 +0200

    feat: removed motion from error paragraph

--- src/components/ui/ContentComponents/ErrorParagraph.tsx
+++ src/components/ui/ContentComponents/ErrorParagraph.tsx
@@ -7,21 +7,15 @@ import { twMerge } from "tailwind-merge";
 interface Props {
   children: ReactNode;
   className?: string;
-  animate?: boolean;
 }
 
-const ErrorParagraph = ({ children, className, animate = true }: Props) => {
+const ErrorParagraph = ({ children, className }: Props) => {
   const error = children;
 
   return (
-    <motion.p
-      initial={animate && { height: 0, opacity: 0 }}
-      animate={animate && { height: "auto", opacity: 1 }}
-      exit={animate ? { height: 0, opacity: 0, padding: 0 } : {}}
-      className={twMerge("text-sm font-medium text-red-500", className)}
-    >
+    <p className={twMerge("text-sm font-medium text-red-500", className)}>
       {error}
-    </motion.p>
+    </p>
   );
 };
 
