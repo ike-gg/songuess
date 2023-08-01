1a1f42b3dfb67340dd36f1adbb6064cb98b544aa
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-01 15:49:24 +0200

    feat: added search bar for set browser

--- src/features/sets/components/SetSelector.tsx
+++ src/features/sets/components/SetSelector.tsx
@@ -5,8 +5,16 @@ import SetListItem from "./SetListItem";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import usePagination from "@/hooks/usePagination";
-import { RxPlus } from "react-icons/rx";
-import { BackButton, Button, Heading, Paragraph } from "@/components/ui";
+import { RxMagnifyingGlass, RxPlus } from "react-icons/rx";
+import {
+  BackButton,
+  Button,
+  Heading,
+  Input,
+  Paragraph,
+  Transition,
+} from "@/components/ui";
+import { current } from "@reduxjs/toolkit";
 
 type Set = Database["public"]["Tables"]["sets"]["Row"];
 
@@ -21,7 +29,9 @@ export interface SetSelectorProps {
 type SetCategory = keyof SetSelectorProps["sets"];
 
 const SetSelector = ({ sets }: SetSelectorProps) => {
+  const [searchOpen, setSearchOpen] = useState(false);
   const [activeSet, setActiveSet] = useState<SetCategory>("featured");
+  const [query, setQuery] = useState("");
 
   const setNames: SetCategory[] = ["featured", "community"];
 
@@ -40,6 +50,16 @@ const SetSelector = ({ sets }: SetSelectorProps) => {
   } = usePagination({
     items: sets[activeSet] ?? [],
     itemsPerPage: 5,
+    query,
+    queryFn: (query: string) => {
+      const items = sets[activeSet];
+      if (!items) return [];
+      return items.filter(
+        (item) =>
+          item.name.toLowerCase().includes(query.toLowerCase()) ||
+          item.description?.toLowerCase().includes(query.toLowerCase())
+      );
+    },
   });
 
   return (
@@ -57,22 +77,45 @@ const SetSelector = ({ sets }: SetSelectorProps) => {
           even create your own!
         </Paragraph>
       </div>
-      <div className="flex gap-2 overflow-auto">
-        {setNames.map((setName) => {
-          return (
-            <button
-              onClick={() => setActiveSet(setName)}
-              key={setName}
-              className={twMerge(
-                "rounded-full bg-zinc-800 p-1 px-3 capitalize text-zinc-500 transition-colors",
-                activeSet === setName && "bg-indigo-600 text-white"
-              )}
-            >
-              {setName}
-            </button>
-          );
-        })}
+      <div className="flex justify-between gap-8 overflow-auto">
+        <div className="flex gap-2">
+          {setNames.map((setName) => {
+            return (
+              <Button
+                onClick={() => setActiveSet(setName)}
+                key={setName}
+                size="small"
+                variant={activeSet === setName ? "primary" : "secondary"}
+                className="capitalize"
+              >
+                {setName}
+              </Button>
+            );
+          })}
+        </div>
+        <div>
+          <Button
+            size="small"
+            variant={searchOpen ? "primary" : "secondary"}
+            onClick={() => {
+              setSearchOpen((p) => !p);
+            }}
+            icon={<RxMagnifyingGlass />}
+          >
+            Search
+          </Button>
+        </div>
       </div>
+      <Transition className="-my-2.5" state={searchOpen}>
+        <Input
+          className="my-2 text-base"
+          icon={<RxMagnifyingGlass />}
+          label={"Search set"}
+          placeholder="Name of set"
+          onInput={(e) => setQuery(e.currentTarget.value)}
+          value={query}
+        />
+      </Transition>
       <div className="flex flex-col gap-3">
         {currentSlice &&
           currentSlice.map((set) => {
@@ -85,16 +128,18 @@ const SetSelector = ({ sets }: SetSelectorProps) => {
             variant="secondary"
             onClick={prevPage}
             disabled={!prevPageAvailable}
-            className="grow"
+            className="shrink"
           >
             Previous
           </Button>
-          <span className="grow-[50] text-center">{currentPage}</span>
+          <Button disabled variant="secondary">
+            {currentPage}
+          </Button>
           <Button
             variant="secondary"
             onClick={nextPage}
             disabled={!nextPageAvailable}
-            className="grow"
+            className="w-min shrink"
           >
             Next
           </Button>
