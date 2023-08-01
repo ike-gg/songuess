29552abf1ac98d2d95d8803bdd7d85418dea2be0
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-01 15:49:06 +0200

    feat: added search filtering to usepagination hook

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,25 +1,38 @@
 import chunkArrayBySize from "@/utils/chunkArrayBySize";
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface PaginationOptions<T> {
   items: Array<T>;
   itemsPerPage: number;
+  query?: string;
+  queryFn?: (query: string) => Array<T>;
 }
 
-const usePagination = <T>({ items, itemsPerPage }: PaginationOptions<T>) => {
+const usePagination = <T>({
+  items,
+  itemsPerPage,
+  query,
+  queryFn,
+}: PaginationOptions<T>) => {
+  const [internalItems, setInternalItems] = useState<Array<T>>(items);
   const [currentPage, setCurrentPage] = useState(0);
   const [currentSlice, setCurrentSlice] = useState<T[]>();
 
+  useEffect(() => {
+    setInternalItems(query && queryFn ? queryFn(query) : items);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [items, query]);
+
   const pagedItems = useMemo(() => {
     setCurrentPage(0);
-    return chunkArrayBySize(items, itemsPerPage);
-  }, [items, itemsPerPage]);
+    return chunkArrayBySize(internalItems, itemsPerPage);
+  }, [internalItems, itemsPerPage]);
 
   useEffect(() => {
     setCurrentSlice(pagedItems[currentPage]);
   }, [currentPage, pagedItems]);
 
-  const paginationAvailable = items.length > itemsPerPage;
+  const paginationAvailable = internalItems.length > itemsPerPage;
 
   const nextAvailable = currentPage + 1 < pagedItems.length;
   const prevAvailable = currentPage - 1 >= 0;
