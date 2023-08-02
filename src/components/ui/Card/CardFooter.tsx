a0ce7d23fc7aeb22b782f5b0c871c10bb1bcbccf
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-02 09:37:23 +0200

    feat: card component minor changes

--- src/components/ui/Card/CardFooter.tsx
+++ src/components/ui/Card/CardFooter.tsx
@@ -10,7 +10,7 @@ const CardFooter = ({ children, className }: Props) => {
   return (
     <div
       className={twMerge(
-        "-m-4 flex flex-col gap-4 overflow-x-auto border-t border-zinc-800 bg-zinc-950/50 p-4 md:-m-6 md:-mt-0 md:p-6",
+        "-m-4 flex flex-col gap-4 overflow-x-auto border-t border-zinc-800 bg-zinc-950/50 p-4 md:-m-6 md:mt-0 md:p-6",
         className
       )}
     >
