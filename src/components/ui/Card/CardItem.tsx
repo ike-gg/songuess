c76397f2e064f4f5c0cd33a9ae9c04f5f8d9a1f6
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-02 09:37:23 +0200

    feat: card component minor changes

--- src/components/ui/Card/CardItem.tsx
+++ src/components/ui/Card/CardItem.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   href?: string;
   children?: ReactNode;
+  className?: string;
 }
 
-const CardItem = ({ href, children }: Props) => {
+const CardItem = ({ href, children, className }: Props) => {
   return (
     <Link
       href={href || ""}
-      className="flex items-center gap-3 rounded-md border border-zinc-700/30 bg-zinc-800/50 p-3 duration-100 hover:border-zinc-800 hover:bg-zinc-800 active:border-zinc-700"
+      className={twMerge(
+        "flex items-center gap-3 rounded-md border border-zinc-700/30 bg-zinc-800/50 p-3 duration-100 hover:border-zinc-800 hover:bg-zinc-800 active:border-zinc-700",
+        className
+      )}
     >
       {children}
     </Link>
   );
 };
 
-export {CardItem};
+export { CardItem };
