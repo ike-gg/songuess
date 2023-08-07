7212341ffdd99077d83d4c8d42631432c3e639df
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-07 10:35:42 +0200

    fix: double spaces

--- src/utils/parseTitleToGuess.ts
+++ src/utils/parseTitleToGuess.ts
@@ -3,14 +3,17 @@ import removeParentheses from "./removeParentheses";
 
 const parseTitleToGuess = (title: string) => {
   const _parsedTitle = removeParentheses(title);
-
   const parsedTitle = parseDiacriticalChars(_parsedTitle);
 
   const symbolsToKeep = ["&"];
-
   const expression = new RegExp(`[^a-zA-Z0-9 ${symbolsToKeep.join("")}]`, "g");
 
-  return parsedTitle.replace(expression, "");
+  const withoutSymbols = parsedTitle.replace(expression, "");
+
+  const expressionForSpaces = /(\s{2,})/g;
+  const withoutSpaces = withoutSymbols.replace(expressionForSpaces, " ");
+
+  return withoutSpaces;
 };
 
 export default parseTitleToGuess;
