b8cb2fd652bc35bcfd6d8c0d89ed38e724dccb22
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-12 23:03:46 +0200

    feat: main game component structure update

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -10,6 +10,9 @@ import Preparing from "./components/stages/Preparing";
 import InGame from "./components/stages/InGame";
 import Results from "./components/stages/Results";
 import Motion from "../../components/providers/Motion";
+import BackgroundImage from "./components/BackgroundImage";
+import parseArtwork from "@/utils/parseArtwork";
+import { AnimatePresence } from "framer-motion";
 
 type Set = Database["public"]["Tables"]["sets"]["Row"];
 
@@ -19,9 +22,12 @@ interface Props {
 }
 
 const Game = ({ setDetails, songs }: Props) => {
-  const { status, set, playlist, currRound } = useAppSelector(
+  const { status, set, playlist, currRound, round } = useAppSelector(
     (state) => state.game
   );
+
+  const { currentSong, status: roundStatus } = round;
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -31,12 +37,29 @@ const Game = ({ setDetails, songs }: Props) => {
     dispatch(gameActions.loadSet({ set: setDetails, tracks: songs }));
   }, [set]);
 
+  let backgroundColor: string = "#09090b";
+  let accentColor: string = "#52525b";
+  if (roundStatus !== "countdown" && currentSong) {
+    const { bgColor, primColor } = parseArtwork(currentSong.attributes.artwork);
+    backgroundColor = bgColor;
+    accentColor = primColor;
+  }
+
   return (
     <Motion duration={0.5}>
+      <AnimatePresence>
+        <BackgroundImage
+          key={backgroundColor + accentColor}
+          c1={backgroundColor}
+          acc={accentColor}
+        />
+      </AnimatePresence>
       <main className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden p-3">
-        {status === "preparing" && <Preparing />}
-        {status === "inprogress" && <InGame />}
-        {status === "ended" && <Results />}
+        <AnimatePresence mode="wait">
+          {status === "preparing" && <Preparing key="preparing_stage" />}
+          {status === "inprogress" && <InGame key="inprogress_stage" />}
+          {status === "ended" && <Results key="ended_stage" />}
+        </AnimatePresence>
       </main>
     </Motion>
   );
