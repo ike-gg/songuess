362679f8807321a33502b2c55f22c1bee8188e6b
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-15 18:39:53 +0200

    feat: new font, chagned type to article

--- src/app/(index)/twitter-image.tsx
+++ src/app/(index)/twitter-image.tsx
@@ -1,15 +1,16 @@
 import { Logo } from "@/components/ui";
 import { ImageResponse } from "next/server";
 
-// Route segment config
 export const runtime = "edge";
 
-// Image metadata
-export const alt = "About Acme";
+export const alt = "SonGuess";
 export const size = {
   width: 1200,
-  height: 630,
+  height: 600,
 };
+
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const contentType = "image/png";
 
@@ -17,18 +18,24 @@ export const contentType = "image/png";
 export default async function Image() {
   // Font
 
+  const interFont = fetch(new URL("./Inter-Medium.ttf", import.meta.url)).then(
+    (res) => res.arrayBuffer()
+  );
+
   return new ImageResponse(
     (
       <div
+        className={inter.className}
         style={{
-          fontSize: 128,
+          fontSize: 64,
           width: "100%",
           height: "100%",
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          alignItems: "flex-end",
+          justifyContent: "flex-start",
           gap: 20,
-          background: "linear-gradient(to bottom right, red,blue)",
+          color: "#4F46E5",
+          background: "radial-gradient(circle at center, #27272A, #18181B)",
         }}
       >
         <Logo />
@@ -40,14 +47,14 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      // fonts: [
-      //   {
-      //     name: "Inter",
-      //     data: await interSemiBold,
-      //     style: "normal",
-      //     weight: 400,
-      //   },
-      // ],
+      fonts: [
+        {
+          name: "Inter",
+          data: await interFont,
+          style: "normal",
+          weight: 400,
+        },
+      ],
     }
   );
 }
