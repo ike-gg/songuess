42092e41c0b47ebeb524ba8f4f189d2a1bc124b2
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-15 23:47:16 +0200

    feat: added meta tags to pages

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -3,16 +3,6 @@ import getPopularAlbums from "@/lib/getPopularAlbums";
 import getPopularSongs from "@/lib/getPopularSongs";
 import { Database } from "@/types/supabase";
 import { createClient } from "@supabase/supabase-js";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "SonGuess",
-  description: "Song guessing game.",
-  themeColor: "#4F46E5",
-  twitter: {
-    card: "summary_large_image",
-  },
-};
 
 const HomePage = async () => {
   const supabase = createClient<Database>(
