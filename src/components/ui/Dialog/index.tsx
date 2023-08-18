2446d73eda957de6910b2730b79212853a39865c
ike <67127723+ike-gg@users.noreply.github.com>
2023-08-18 22:23:26 +0200

    feat: focus trap on dialog modal

--- src/components/ui/Dialog/index.tsx
+++ src/components/ui/Dialog/index.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { ReactNode } from "react";
 import { Card } from "../Card";
 import Motion from "@/components/providers/Motion";
+import FocusTrap from "focus-trap-react";
 
 interface Props {
   children?: ReactNode;
@@ -16,24 +17,27 @@ const Dialog = ({ handleState, state, children }: Props) => {
     <Motion duration={0.3}>
       <AnimatePresence>
         {state && (
-          <motion.div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center">
-            <motion.div
-              onClick={() => handleState(false)}
-              className="absolute h-full w-full bg-black/75"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            />
-            <motion.div
-              className="p-4"
-              onClick={(e) => e.stopPropagation()}
-              initial={{ opacity: 0, scale: 0.95 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.95 }}
-            >
-              <Card className="w-full md:max-w-xl">{children}</Card>
-            </motion.div>
-          </motion.div>
+          <FocusTrap>
+            <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center">
+              <motion.div
+                onClick={() => handleState(false)}
+                className="absolute h-full w-full bg-black/75"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+
+              <motion.div
+                className="p-4"
+                onClick={(e) => e.stopPropagation()}
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.95 }}
+              >
+                <Card className="w-full md:max-w-xl">{children}</Card>
+              </motion.div>
+            </div>
+          </FocusTrap>
         )}
       </AnimatePresence>
     </Motion>
